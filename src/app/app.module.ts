e6ff62b1efc8e274829ccad50d1e7ae5ee82a1fb
NurlashKO <zh.nurlan96@gmail.com>
2017-03-04 19:07:12 +0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { FkNavbarComponent } from './components/fk-navbar/fk-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FkNavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    MaterialModule,
     HttpModule
   ],
   providers: [],
---
+++ src/app/components/fk-navbar/fk-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fk-navbar',
+  templateUrl: './fk-navbar.component.html',
+  styleUrls: ['./fk-navbar.component.css']
+})
+export class FkNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
