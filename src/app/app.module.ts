154df95ae238c989bd88bbc0507ee91baffe01c1
NurlashKO <zh.nurlan96@gmail.com>
2017-03-08 20:49:40 +0600

    added news components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,30 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { FkNavbarComponent } from './components/fk-navbar/fk-navbar.component';
 import { FkSidenavComponent } from './components/fk-sidenav/fk-sidenav.component';
+import { FkNewsCardComponent } from './components/fk-news-card/fk-news-card.component';
+
+import { PostService } from './components/fk-news-card/post.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     FkNavbarComponent,
-    FkSidenavComponent
+    FkSidenavComponent,
+    FkNewsCardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     MaterialModule,
+    FlexLayoutModule.forRoot(),
     HttpModule
   ],
-  providers: [],
+  providers: [PostService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/fk-news-card/post.service.ts
@@ -0,0 +1,17 @@
+import { Post } from './post';
+
+import { Injectable } from '@angular/core';
+
+export let POSTS: Post[] = [
+  {id: 11, title: 'Post #1', content: 'Content 1'},
+  {id: 12, title: 'Post #2', content: 'Content 2'},
+  {id: 13, title: 'Post #3', content: 'Content 3'},
+];
+
+@Injectable()
+export class PostService {
+  getPosts(): Promise<Post[]> {
+    return Promise.resolve(POSTS);
+  }
+}
+
