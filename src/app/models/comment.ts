54caed8efd90bfaf247cd47bbcf3035bee735761
Ильяс Сабешулы < >
2017-04-07 04:10:49 +0600

    deleted

--- src/app/models/comment.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "./user";
-
-export interface IComment {
-    id?: number;
-    author: User;
-    publish_date: string;
-    content: string;
--- src/app/models/user.ts
+++
@@ -1,9 +0,0 @@
-
-export class User {
-    id: number;
-    username:string;
-    email: string;
-    password: string;    
-    firstName: string;
-    lastName: string;
-}
