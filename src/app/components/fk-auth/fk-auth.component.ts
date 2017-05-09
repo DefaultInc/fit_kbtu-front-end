da0cf59520cbab315f16e123aa82b02067aa737e
NurlashKO <zh.nurlan96@gmail.com>
2017-05-09 18:33:40 +0600

    Added reactive forms with validation to signup action.

--- src/app/components/fk-auth/fk-auth.component.ts
+++ src/app/components/fk-auth/fk-auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { MdDialogRef, MD_DIALOG_DATA } from '@angular/material';
 import { AuthenticationService } from '../../services/authentication.service';
@@ -9,20 +10,31 @@ import { AuthenticationService } from '../../services/authentication.service';
   styleUrls: ['./fk-auth.component.css']
 })
 export class FkLoginComponent implements OnInit {
-  
   model: any = {};
   loading = false;
   returnUrl: string;
   focusTab: number;
-  isLoginForm: boolean
+  isLoginForm: boolean;
+  signupForm: FormGroup;
   
   constructor(
         private authenticationService: AuthenticationService,
         public dialogRef: MdDialogRef<FkLoginComponent>,
+        private fb: FormBuilder,
         @Inject(MD_DIALOG_DATA) public data: any) {
+            this.createForm()
             this.isLoginForm = data == 'login' ? true : false;
         }
-  
+
+  createForm() {
+    this.signupForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]]
+    });
+    this.signupForm.valueChanges
+      .subscribe(data => {});
+  }
     
   ngOnInit() {
   }
@@ -39,19 +51,24 @@ export class FkLoginComponent implements OnInit {
                     this.closeDialog();
                 },
                 error => {
+                    this.model.errors = error
                     this.loading = false;
                 });
     }
 
 signUp() {
+    if (this.signupForm.status == "INVALID") return;
+      let model = this.signupForm.value
+      console.log(model)
       this.loading = true;
-      this.authenticationService.signUp(this.model.username, this.model.password)
+      this.authenticationService.signUp(model.username, model.password)
           .subscribe(
               data => {
                 this.login();
                 this.closeDialog();
               },
               error => {
+                  this.model.errors = error
                   this.loading = false;
               });
   }
